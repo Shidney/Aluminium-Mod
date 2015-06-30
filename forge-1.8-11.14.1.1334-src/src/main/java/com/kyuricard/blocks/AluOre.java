e9238f58f1c839e3debeaca4bf06d70ab8b322e9
Shidney <shidneydd@hotmail.com>
2015-06-30 23:25:26 +0200

    Update AluOre.java

--- forge-1.8-11.14.1.1334-src/src/main/java/com/kyuricard/blocks/AluOre.java
+++ forge-1.8-11.14.1.1334-src/src/main/java/com/kyuricard/blocks/AluOre.java
@@ -1,5 +1,5 @@
 package com.kyuricard.blocks;
-
+//modified by shidney
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
